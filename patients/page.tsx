292f406d6511ad4294886ac255505470e57954e5
leonie777-gif <lgerinter@gmail.com>
2026-02-13 16:22:18 +0100

    Add files via upload

--- patients/page.tsx
+++ patients/page.tsx
@@ -1,1 +1,16 @@
+"use client";
 
+import React from "react";
+
+export default function Patients() {
+  React.useEffect(() => {
+    // Zu Home weiterleiten
+    window.location.href = "/home";
+  }, []);
+
+  return (
+    <div className="min-h-screen bg-slate-50 flex items-center justify-center">
+      <div className="text-slate-600">Wird weitergeleitet...</div>
+    </div>
+  );
+}
