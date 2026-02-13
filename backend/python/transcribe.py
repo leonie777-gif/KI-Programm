8397e70de107c6d951fbfcee7ec7d4b6e03942b6
leonie777-gif <lgerinter@gmail.com>
2026-02-13 16:29:17 +0100

    Add files via upload

--- backend/python/transcribe.py
+++ backend/python/transcribe.py
@@ -1,1 +1,17 @@
+import os
+os.environ["MKL_VERBOSE"] = "0"
+os.environ["MKL_WARNINGS"] = "0"
 
+import sys
+from faster_whisper import WhisperModel
+
+def main():
+    audio_path = sys.argv[1]
+    model = WhisperModel("base")
+    segments, info = model.transcribe(audio_path, language="de")
+
+    text = " ".join(seg.text for seg in segments)
+    print(text)
+
+if __name__ == "__main__":
+    main()
